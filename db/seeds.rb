50ed8dd36e0ed3d0757990b4d0fe803165cded6a
Marius Hepp <marius.hepp@gmail.com>
2021-08-25 18:07:50 +0200

    mapbox incl. design, window, marker (dont show) + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,9 @@ Event.destroy_all
 
 user_1 = User.create!(
   email: "test@joinsporty.com",
-  fullname: "123",
-  password: "123"
+  first_name: "test",
+  last_name: "test",
+  password: "123456"
 )
 
 # 01
