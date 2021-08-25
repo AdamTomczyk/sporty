91dc468521a997fad70bcf173f9124d4ff2f6d7a
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-08-25 17:35:02 +0200

    show page done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,9 +23,15 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import "animate.css"
+
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+
+
+
 
 // Canvas
 function canvas() {
@@ -108,4 +114,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   canvas()
+
 });
