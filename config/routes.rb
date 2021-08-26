9a283a6fae3ba523f08bbd83550cab17e959e5ec
may dang <dang.dieutrang@gmail.com>
2021-08-26 18:27:18 +0200

    changed /dashboard to /games

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get "/dashboard", to: "join_requests#index", as: :join_requests
+  get "/games", to: "join_requests#index", as: :join_requests
   # resources: default + _path, as : through join_requests to get to that page
 
   resources :chatrooms do
