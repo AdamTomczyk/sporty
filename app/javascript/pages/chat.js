f30fba9748b584a600dbd33384b9af0a0c841625
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-09-01 14:20:21 +0200

    chat done ✅

--- app/javascript/pages/chat.js
+++ app/javascript/pages/chat.js
@@ -2,12 +2,22 @@
 const showChatBox = () => {
 
   const chatButton = document.querySelector(".chatbtn");
+  if (chatButton) {
   const chatbox = document.querySelector(".chatbox");
+  const accordion = document.querySelector(".accordion")
+  const leavebutton = document.querySelector(".leavebutton")
 
     chatButton.addEventListener("click", (event) => {
       // event.preventDefault
-      chatbox.classList.toggle("showchat");
+      chatbox.classList.add("showchat");
+      accordion.classList.add("hide")
     })
+
+  leavebutton.addEventListener("click", () => {
+    chatbox.classList.remove("showchat");
+    accordion.classList.remove("hide");
+  })
+}
 
 }
 
